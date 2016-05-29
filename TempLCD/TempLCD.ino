50ec3e64c8039631e6cbcb14d35eaf4328708987
Skoky <skokys@gmail.com>
2016-05-29 17:08:40 +0200

    improved display and new photo

--- TempLCD/TempLCD.ino
+++ TempLCD/TempLCD.ino
@@ -3,6 +3,7 @@
 #include <LiquidCrystal.h>
 #include "Bridge.h"
 #include "HttpClient.h"
+#include <Process.h>
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -14,35 +15,21 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);           // select the pins used on the LC
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
-byte smiley[8] = {
-  B00000,
-  B10001,
-  B00000,
-  B00000,
-  B10001,
-  B01110,
-  B00000,
-};
-
 //ThingSpeak Settings 
 String thingSpeakAPI = "api.thingspeak.com";
 String talkBackAPIKey = "R2EIQA5ZNL2BMMRN";
 String talkBackID = "YYYY";
-const int checkTalkBackInterval = 15 * 1000;    // Time interval in milliseconds to check TalkBack (number of seconds * 1000 = interval)
+const int checkTalkBackInterval = 3 * 1000;    // Time interval in milliseconds to check TalkBack (number of seconds * 1000 = interval)
 
 // Variable Setup
 long lastConnectionTime = 0;
 
 void setup(void)
-{
-  // start serial port
-  Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
-  lcd.createChar(0, smiley);
+{ 
   lcd.begin(16, 2);               // start the library
   lcd.setCursor(0,0);             // set the LCD cursor   position
-  lcd.print("Teplota");  // print a simple message on the LCD
-
+  lcd.print("Init ...");  // print a simple message on the LCD
+  
   // Start up the library
   sensors.begin();
   
@@ -50,35 +37,71 @@ void setup(void)
   Bridge.begin();
   
   // Initialize Serial
-//  Serial.begin(9600);
-//  while(!Serial);
+  Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();  // make contact with the linux processor
+  digitalWrite(13, HIGH);  // Led on pin 13 turns on when the bridge is ready
+  delay(2000);  // wait 2 seconds
+  lcd.setCursor(0,0);             // set the LCD cursor   position
+  lcd.print("Temp ...");
 }
 
 void loop(void)
 { 
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-//  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-//  Serial.println("DONE");
-  delay(1000);
-//  Serial.print("Temperature for the device 1 (index 0) is: ");
-  float teplota = sensors.getTempCByIndex(0);
-//  Serial.println(teplota);  
+  float temp = getTemp();
+
+  lcd.setCursor(5,0);
+  lcd.print("    ");
+  lcd.setCursor(5,0);
+  lcd.print(temp);  
 
   lcd.setCursor(0,1);
-  lcd.print(teplota);  
-  lcd.print(" ");
-  lcd.write(byte(0));  
-//  lcd.noDisplay();
- checkTalkBack(teplota);
- delay(checkTalkBackInterval);
+  lcd.print("Wifi...");
+  String wifi = checkWifi();
+  lcd.setCursor(0,1);
+  lcd.print(wifi);
+
+  lcd.setCursor(12,1);
+  lcd.print("->...");
+  post(temp);
+  lcd.setCursor(12,1);
+  lcd.print("->Ok");
+  delay(checkTalkBackInterval); 
+} 
+
+float getTemp() 
+{
+// call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  // Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  delay(1000);
+  Serial.print("Temperature for the device 1 (index 0) is: ");
+  float temp = sensors.getTempCByIndex(0);
+  Serial.println(temp);  
+  return temp;
 }
 
-void checkTalkBack(float temp)
+String checkWifi() 
 {
-  lcd.setCursor(10,1);
-  lcd.print("       ");
+  Process wifiCheck;  // initialize a new process
+  wifiCheck.runShellCommand("/usr/bin/pretty-wifi-info.lua | grep Signal");  // command you want to run
+
+  String label = "";
+  while (wifiCheck.available() > 0) {
+    char c = wifiCheck.read();
+    SerialUSB.print(c);
+    label += c;
+  }
+  label.trim();
+  Serial.println(label);
+  return label;
+
+}
+void post(float temp)
+{
   HttpClient client;
   
   String talkBackCommand;
@@ -92,6 +115,4 @@ void checkTalkBack(float temp)
     charIn = client.read();
     talkBackCommand += charIn;
   }
-  lcd.setCursor(10,1);
-  lcd.print("D:"+talkBackCommand);  
 }
